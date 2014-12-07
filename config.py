72e336e51cbab695c2575dcb9107eeff52385018
tiago <krebstiago@gmail.com>
2014-12-07 09:45:55 -0200

    Final exemplo de estrutura modular

--- config.py
+++ config.py
@@ -1,42 +1,45 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Configuracao geral
+
 class Config:
-  SECRET_KEY = os.environ.get('SECRET_KEY') or 'd9b773cd3f4dccf50a372ccfd34199b3'
-  SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-  PP_MAIL_SUBJECT_PREFIX = '[PP]'
-  PP_MAIL_SENDER = 'PP Admin <admin@pp.com.br>'
-  PP_ADMIN = os.environ.get('PP_ADMIN')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
-  @staticmethod
-  def init_app(app):
-    pass
+    @staticmethod
+    def init_app(app):
+        pass
 
-# Cinfiguracao de ambientes
+
 class DevelopmentConfig(Config):
-  DEBUG = True
-  MAIL_SERVER = 'smtp.googlemail.com'
-  MAIL_PORT = 587
-  MAIL_USE_TLS = True
-  MAIL_USERNAME = os.enviton.get('MAIL_USERNAME')
-  MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-  SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-    'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
-    
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
 class TestingConfig(Config):
-  TESTING = True
-  SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-    'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
 
 class ProductionConfig(Config):
-  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-  'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
 
 config = {
-  'development': DevelopmentConfig,
-  'testing': TestingConfig,
-  'production': ProductionConfig,
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
 
-  'default': DevelopmentConfig
+    'default': DevelopmentConfig
 }
