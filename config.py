93e585efc429b8552b34f4b996406e4f8f4c0344
tiago <krebstiago@gmail.com>
2014-12-09 19:58:04 -0200

    Final PP-3

--- config.py
+++ config.py
@@ -3,16 +3,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SECRET_KEY = os.environ.get('PP_SECRET_KEY') or 'd9b773cd3f4dccf50a372ccfd34199b3'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    MAIL_USERNAME = os.environ.get('PP_MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('PP_MAIL_PASSWORD')
+    PP_MAIL_SUBJECT_PREFIX = '[PP]'
+    PP_MAIL_SENDER = 'PP Admin <admin@pp.com.br>'
+    PP_ADMIN = os.environ.get('PP_ADMIN')
 
     @staticmethod
     def init_app(app):
@@ -21,18 +21,18 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('PP_DEV_DATABASE_URL') #or \
+        #'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('PP_TEST_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('PP_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
 
--- manage.py
+++ manage.py
@@ -5,10 +5,9 @@ from app.models import User, Role
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app(os.getenv('PP_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
-
 
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
